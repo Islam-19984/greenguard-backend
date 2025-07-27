1edf8df873a09cac204e21857aea7d4aa5d810b9
Islam-19984 <120138935+Islam-19984@users.noreply.github.com>
2025-07-27 02:41:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ def rate_limit_check(identifier):
     return True
 
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
+MONGO_URI = os.getenv("MONGODB_URI", "mongodb://localhost:27017/")
 DATABASE_NAME = os.getenv("DATABASE_NAME", "greenguard_db")
 
 try:
